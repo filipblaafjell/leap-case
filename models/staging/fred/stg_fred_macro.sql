0c9e60ffe73fbf29768df51628b40c558a6a3a01
Filip Blaafjell <fbl@veo.co>
2025-10-19 17:42:19 +0200

    Added staging models and schemas for FRED datasets

---
+++ models/staging/fred/stg_fred_macro.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='view') }}
+
+with cleaned as (
+    select
+        try_to_date(date) as date,
+        try_cast(ces0500000003 as float) as avg_hourly_earnings,
+        try_cast(cpiaucsl as float) as cpi_all_items,
+        try_cast(cusr0000saf11 as float) as food_price_index,
+        try_cast(cusr0000setb01 as float) as gasoline_price_index,
+        try_cast(ecomnsa as float) as ecom_sales,
+        try_cast(fedfunds as float) as fed_funds_rate,
+        try_cast(jtsjol as float) as job_openings,
+        try_cast(pce as float) as personal_consumption,
+        try_cast(pi as float) as personal_income,
+        try_cast(psavert as float) as personal_saving_rate,
+        try_cast(revolsl as float) as credit_card_debt,
+        try_cast(rsafs as float) as retail_sales,
+        try_cast(umcsent as float) as consumer_sentiment
+    from {{ source('raw_fred', 'FRED_MACRO_RAW') }}
+    where date is not null
+)
+
+select * from cleaned
---
+++ models/staging/fred/stg_fred_unemp.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+with cleaned as (
+    select
+        try_to_date(date) as date,
+        upper(trim(state)) as state,
+        try_cast(unemployment_rate as float) as unemployment_rate
+    from {{ source('raw_fred', 'FRED_UNEMPLOYMENT_STATE_RAW') }}
+    where date is not null
+)
+
+select *
+from cleaned
